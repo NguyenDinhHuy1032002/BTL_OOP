67f59065da71ced0d0c88f34734b90eebcd5ced6
NguyenDinhHuy1032002 <81515703+NguyenDinhHuy1032002@users.noreply.github.com>
2021-10-30 15:12:25 +0700

    Update NhanVien.cpp

--- NhanVien.cpp
+++ NhanVien.cpp
@@ -1,6 +1,33 @@
 #include "NhanVien.hpp"
 using namespace std;
 
+// Ngay
+// phuong thuc getter cua ngay
+int Ngay::Getter_Ngay(){
+    return ngay;
+}
+// phuong thuc setter cua ngay
+int Ngay::Setter_Ngay(int NGAY) {
+    ngay =  NGAY;
+}
+// phuong thuc getter cua thang
+int Ngay::Getter_Thang() {
+	return thang
+}
+// phuong thuc setter cua thang
+int Ngay::Setter_Thang(int THANG) {
+	thang = THANG;
+}
+// phuong thuc getter cua nam
+int  Ngay::Getter_Nam() {
+	return nam;
+}
+// phuong thuc setter cua nam
+int Ngay::Setter_Nam(int NAM) {
+	nam = NAM;
+}
+
+// Nhan Vien
 NhanVien::NhanVien() {
     Ten = "";
     Ma = "";
@@ -9,29 +36,91 @@ NhanVien::NhanVien() {
     NgaySinh.thang = 0;
     NgaySinh.nam = 0;
     LuongCoBan = 0;
+    NamLamViec = 0;
+    SoNgayNghi = 0;
 }
 NhanVien::~NhanVien(){}
 
-void NhanVien::Nhap() {
-    cout << "Nhap ho ten: ";
-    getline(cin, Ten);
-    cout << "Nhap ma nhan vien: ";
-    getline(cin , Ma);
-    cout << "Nhap noi sinh: ";
-    getline(cin, NoiSinh);
-    cout << "Nhap ngay sinh: ";
-    cin >> NgaySinh.ngay >> NgaySinh.thang >> NgaySinh.nam;
-    cout << "Nhap luong co ban: ";
-}
-
-void NhanVien::Xuat() {
-    cout << "Ho ten: " << Ten << endl;
-    cout << "Ma nhan vien: " << Ma << endl;
-    cout << "Noi sinh: " << endl;
-    cout << "Ngay sinh: " << NgaySinh.ngay << "/" << NgaySinh.thang << "/" << NgaySinh.nam << endl;
-    cout << "Luong co ban: " << LuongCoBan << endl;
+void NhanVien::Doc_File(ifstream &filein) {
+    getline(filein, Ten, ',');
+    filein.seekg(1, 1); // dich con tro chi vi cua file tai vi tri hien tai sang ben phai 1 byte
+	getline(filein, Ma, ',');
+	filein.seekg(1, 1);
+	getline(filein, NoiSinh, ',');
+	int ngay;
+	filein >> ngay;
+	NgaySinh.Setter_Ngay(ngay);
+	filein.seekg(1, 1);
+	int thang;
+	filein >> thang;
+	NgaySinh.Setter_Thang(thang);
+	filein.seekg(1, 1);
+	int nam;
+	filein >> nam;
+	NgaySinh.Setter_Nam(nam);
+	filein.seekg(2, 1);
+	filein >> LuongCoBan;
+	filein.seekg(1, 1);
+	filein >> NamLamViec;
+	filein.seekg(1, 1);
+	filein >> SoNgayNghi;
 }
 
 float NhanVien::getLuong() {
-    return this->TinhLuong();
+    return this->LuongCoBan;
+}
+
+int NhanVien::getNamLamViec() {
+	return this->NamLamViec;
+}
+
+int NhanVien::getSoNgayNghi() {
+	return this->SoNgayNghi;
+}
+
+// Nhan Vien San Xuat
+void NhanVienSanXuat::Doc_File(ifstream &filein) {
+	NhanVien::Doc_File(filein);
+	filein >> DonGiaMotSanPham;
+	filein.seekg(1, 1);
+	filein >> SoSanPham;
+}
+
+float NhanVienSanXuat::TinhLuong() {
+	float Luong = 0;
+	Luong = DonGiaMotSanPham*SoSanPham - getSoNgayNghi()*DonGiaMotSanPham*SoSanPham;
+	if (getNamLamViec() >= 1 && getNamLamViec() <= 4) {
+		Luong += 4500000;
+	}
+	if (getNamLamViec() <= 10) {
+		Luong += 8000000;
+	}
+	else {
+		Luong += 15000000;
+	}
+	return Luong;
+}
+
+// Nhan Vien Van Phong
+void NhanVienVanPhong::Doc_File(ifstream &filein) {
+	NhanVien::Doc_File(filein);
+	filein >> HeSoLuong;
+	filein.seekg(1, 1);
+	filein >> SoNgayLam;
+}
+
+
+float NhanVienVanPhong::TinhLuong() {
+	float Luong = 0;
+	Luong = HeSoLuong*SoNgayLam(30 - getSoNgayNghi());
+	if (getNamLamViec() >= 1 && getNamLamViec() <= 4) {
+		Luong +=4500000;
+	}
+	if (getNamLamViec() <= 10) {
+		Luong += 8000000;
+	}
+	else {
+		Luong += 15000000;
+	}
+	return Luong;
 }
